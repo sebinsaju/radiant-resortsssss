c9348aef808883b49858a37ec64388b5a0306bcf
sebinsaju <65224759+sebinsaju@users.noreply.github.com>
2022-06-26 11:42:16 +0530

    build

--- pages/packages/index.js
+++ pages/packages/index.js
@@ -3,13 +3,13 @@ import React from 'react'
 const Packages = () => {
     const DATA = [
         {
-            title: "Buffet Lunch Offer", image: "/packages-1.jpg", desc: "Sip, snack and enjoy mouth-watering luncheon in buffet style with our exclusive Buffet Lunch Offer at the best rate!", link: "/packages/package/buffet-lunch"
+            title: "Buffet Lunch Offer", image: "/packages-1.jpg", desc: "Sip, snack and enjoy mouth-watering luncheon in buffet style with our exclusive Buffet Lunch Offer at the best rate!", link: "/package/buffet-lunch"
         },
         {
-            title: "Exclusive Breakfast & Lunch Buffet Offer", image: "/packages-2.jpg", desc: "Indulge in varieties of delicious cuisines with our amazing Breakfast & Lunch Buffet Offer at Radiant Resort Bangalore.Hurry Now!", link: "/packages/package/exclusive-breakfast"
+            title: "Exclusive Breakfast & Lunch Buffet Offer", image: "/packages-2.jpg", desc: "Indulge in varieties of delicious cuisines with our amazing Breakfast & Lunch Buffet Offer at Radiant Resort Bangalore.Hurry Now!", link: "/package/exclusive-breakfast"
         },
-        { title: "All-Day Dining Offer", image: "/packages-3.jpg", desc: "Feast your taste buds with appetizing cuisines on a spread of buffet options at Breakfast, Lunch, and Dinner at best rate ever!.", link: "/packages/package/all-day" },
-        { title: "Couples' Buffet Offer", image: "/packages-4.jpg", desc: "Enjoy a romantic vacation with your loved one at Radiant Resort Bangalore and dine in buffet style at Breakfast, Lunch, and Dinner at the best rate", link: "/packages/package/couple-buffet" },
+        { title: "All-Day Dining Offer", image: "/packages-3.jpg", desc: "Feast your taste buds with appetizing cuisines on a spread of buffet options at Breakfast, Lunch, and Dinner at best rate ever!.", link: "/package/all-day" },
+        { title: "Couples' Buffet Offer", image: "/packages-4.jpg", desc: "Enjoy a romantic vacation with your loved one at Radiant Resort Bangalore and dine in buffet style at Breakfast, Lunch, and Dinner at the best rate", link: "/package/couple-buffet" },
     ]
     return (
         <div><section className="w3l-breadcrumb">
